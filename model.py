128be1c0c14e573e3de9bc79e6010140e1164c01
Yonghye Kwon <developer.0hye@gmail.com>
2021-11-15 22:17:07 +0900

    Update skeleton class for VLF

--- model.py
+++ model.py
@@ -1,1 +1,22 @@
+import torch
+from torch import nn
 
+import timm
+
+from einops import rearrange, repeat
+from einops.layers.torch import Rearrange
+
+class VideoLightFormer(nn.Module):
+  def __init__(self, 
+               backbone_model_name,
+               num_classes):
+    super().__init__()
+    
+    self.f = nn.Sequential()
+    self.g = None
+    self.h = None
+    self.mlp = None
+  
+  def forward(self, x):
+    return self.mlp(self.h(self.g(self.f(x))))
+  
