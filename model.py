ed079d7882ab8133566c73b4b8f6303a4522d9fd
Yonghye Kwon <developer.0hye@gmail.com>
2021-11-17 01:18:33 +0900

    explicitly set embed_dim

--- model.py
+++ model.py
@@ -10,6 +10,7 @@ class Transformer(nn.Module):
   def __init__(self, 
                num_encoders,
                num_heads,
+               embed_dim,
                mlp_ratio=4.0):
     super().__init__()
   def forward(self, x):
@@ -31,20 +32,25 @@ class VideoLightFormer(nn.Module):
     backbone_out_channels = self.backbone.feature_info.channels()[-1]
 
     self.conv = nn.Conv2d(in_channels=backbone_out_channels,
-                          out_channels=256,
+                          out_channels=embed_dim,
                           kernel_size=3,
                           stride=2)
     
-    self.bn = nn.BatchNorm2d(256)
+    self.bn = nn.BatchNorm2d(embed_dim)
     
-    self.g = Transformer(num_encoders=1, num_heads=8)
+    self.g = Transformer(num_encoders=1, 
+                         num_heads=8,
+                         embed_dim=embed_dim)
+    
     self.h = nn.Sequential(Rearrange('(b t) n c -> b (t n) c', t=num_frames),
                            Transformer(num_encoders=4,
-                                       num_heads=8))
+                                       num_heads=8,
+                                       embed_dim=embed_dim))
+    
     self.mlp = nn.Sequential(Rearrange('b tn c -> b c tn'),
                              nn.AdaptiveAvgPool1d(1),
                              nn.Flatten(start_dim=1),
-                             nn.Linear(256, num_classes))
+                             nn.Linear(embed_dim, num_classes))
   
   def f(self, x):
     x = self.backbone.forward(x)[-1]
