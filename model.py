33bdd93352afbbaea891ce4d92a34812e47a2852
Yonghye Kwon <developer.0hye@gmail.com>
2021-11-17 01:16:36 +0900

    Implement f(x) and MLP(x)

--- model.py
+++ model.py
@@ -6,17 +6,53 @@ import timm
 from einops import rearrange, repeat
 from einops.layers.torch import Rearrange
 
-class VideoLightFormer(nn.Module):
+class Transformer(nn.Module):
   def __init__(self, 
-               backbone_model_name,
-               num_classes):
+               num_encoders,
+               num_heads,
+               mlp_ratio=4.0):
     super().__init__()
+  def forward(self, x):
+    return x
+
+class VideoLightFormer(nn.Module):
+  def __init__(self,
+               num_classes,
+               backbone_model_name="tf_efficientnet_b0_ns",
+               embed_dim=256,
+               num_frames=16,
+               ):
+    super().__init__()
+
+    self.backbone = timm.create_model(model_name=backbone_model_name,
+                                      pretrained=True,
+                                      features_only=True)
     
-    self.f = nn.Sequential()
-    self.g = None
-    self.h = None
-    self.mlp = None
+    backbone_out_channels = self.backbone.feature_info.channels()[-1]
+
+    self.conv = nn.Conv2d(in_channels=backbone_out_channels,
+                          out_channels=256,
+                          kernel_size=3,
+                          stride=2)
+    
+    self.bn = nn.BatchNorm2d(256)
+    
+    self.g = Transformer(num_encoders=1, num_heads=8)
+    self.h = nn.Sequential(Rearrange('(b t) n c -> b (t n) c', t=num_frames),
+                           Transformer(num_encoders=4,
+                                       num_heads=8))
+    self.mlp = nn.Sequential(Rearrange('b tn c -> b c tn'),
+                             nn.AdaptiveAvgPool1d(1),
+                             nn.Flatten(start_dim=1),
+                             nn.Linear(256, num_classes))
+  
+  def f(self, x):
+    x = self.backbone.forward(x)[-1]
+    x = self.conv(x)
+    x = self.bn(x)
+
+    x = rearrange(x, 'b c h w -> b (h w) c')
+    return x
   
   def forward(self, x):
     return self.mlp(self.h(self.g(self.f(x))))
-  
